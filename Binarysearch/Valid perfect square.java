fa466ce6adbaf996c45b747b6fb6a32931610c9c
Himanshu Kumar <70852622+hkad-102@users.noreply.github.com>
2022-03-23 01:17:21 +0530

    Create Valid perfect square.java

---
+++ Binarysearch/Valid perfect square.java
@@ -0,0 +1,17 @@
+class Solution{
+  public boolean isPerfectSquare(int num){
+    int low = 0;
+    int high = num;
+    while(low <= high){
+      int mid = low + (high - low)/2;
+      if(mid * mid == num){
+        return true;
+      }else if(mid * mid < num){
+        low = mid + 1;
+      }else{
+        high = mid - 1;
+      }
+    }
+    return false;
+  }
+}
