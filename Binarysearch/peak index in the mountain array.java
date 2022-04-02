2ce22b5e112bf71f82f7da0d484ea5953fe98703
Himanshu Kumar <70852622+hkad-102@users.noreply.github.com>
2022-04-02 20:00:50 +0530

    Update peak index in the mountain array.java

--- Binarysearch/peak index in the mountain array.java
+++ Binarysearch/peak index in the mountain array.java
@@ -7,7 +7,7 @@
 * arr[i] > arr[i+1] > ... > arr[arr.length - 1]
 * Given an integer array arr that is guaranteed to be a mountain, 
 * return any i such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
-
+  [ 1 2 3 4 5 6 4 3 2 1 ] let say we are on 3 than 3 is greater than 2 than we have to go to right side to get 6
 */
 
 class Solutions{
@@ -16,7 +16,8 @@ class Solutions{
     int high = arr.length - 1;
     while(low < high){
       int mid = low + (high - low)/2;
-      if(arr[mid] > arr[mid - 1]){
+      //if the middle element is greater than the mid + 1 than find the peak in the left side 
+      if(arr[mid] > arr[mid + 1]){
         high = mid;
       }else{
         low = mid + 1;
