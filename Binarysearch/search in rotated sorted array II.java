791527bc30570e8f011a1d6af3d94042741f8440
Himanshu Kumar <70852622+hkad-102@users.noreply.github.com>
2022-04-08 12:52:59 +0530

    Update search in rotated sorted array II.java

--- Binarysearch/search in rotated sorted array II.java
+++ Binarysearch/search in rotated sorted array II.java
@@ -18,4 +18,29 @@ Example 1:
 Input: nums = [2,5,6,0,0,1,2], target = 0
 Output: true
 */
-class Solution
+class Solution{
+ public boolean search(int nums[], int target){
+  int low = 0;
+  int high = nums.length - 1;
+  while(low <= high){
+   int mid = low + (high - low);
+   if(nums[mid] == target) return true;
+   if(nums[mid] == nums[high]) high--;
+   else if(nums[mid] < nums[high]){
+    if(target > nums[mid] && target <= nums[high]){
+     low = mid + 1;
+    }else{
+     high = mid - 1;
+    }
+   }else{
+    if(target < nums[mid] && target >= nums[low]){
+     high = mid - 1;
+    }else{
+     low = mid + 1;
+    }
+   }
+  }
+  return false;
+ }
+}
+       
